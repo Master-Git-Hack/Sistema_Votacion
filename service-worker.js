ed0a917024c7f789e93deb21634dd44ecf1b0e66
Danny Sernna <dany_serna@hotmail.es>
2019-03-24 12:34:14 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/sistema_votacion/precache-manifest.a96e3a1a4a61b49b72e8f5fc76612858.js"
+  "/Sistema_Votacion/precache-manifest.0563ffcdb50a5f60a2ce80a50438e820.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/sistema_votacion/index.html", {
+workbox.routing.registerNavigationRoute("/Sistema_Votacion/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
